a2022f21034b74344f82809fa3f77d259032cac9
esteban <esteban1009@naver.com>
2022-01-30 11:31:00 +0900

    init

---
+++ src/main/java/com/jpaBoard/study/jpaBoardStudy/JpaBoardStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jpaBoard.study.jpaBoardStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpaBoard/study/jpaBoardStudy/JpaBoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpaBoard.study.jpaBoardStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
