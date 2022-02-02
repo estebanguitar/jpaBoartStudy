505b48ecb0d4884a48c08b51272cd48ef727239a
esteban <esteban1009@naver.com>
2022-02-02 19:04:55 +0900

    dev Board update, validation, server-side form validation

--- src/main/java/com/jpaBoard/study/jpaBoardStudy/controller/BoardController.java
+++ src/main/java/com/jpaBoard/study/jpaBoardStudy/controller/BoardController.java
@@ -2,25 +2,30 @@ package com.jpaBoard.study.jpaBoardStudy.controller;
 
 import com.jpaBoard.study.jpaBoardStudy.entity.Board;
 import com.jpaBoard.study.jpaBoardStudy.repsitory.BoardRepository;
+import com.jpaBoard.study.jpaBoardStudy.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
     private BoardRepository boardRepository;
+    private BoardValidator boardValidator;
+
 
     @Autowired
-    public BoardController(BoardRepository boardRepository) {
+    public BoardController(BoardRepository boardRepository,
+                           BoardValidator boardValidator) {
         this.boardRepository = boardRepository;
+        this.boardValidator = boardValidator;
     }
 
     @GetMapping("/list")
@@ -31,13 +36,25 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        }else {
+            model.addAttribute("board", boardRepository.findById(id).orElse(null));
+        }
+
         return "board/form";
     }
 
     @PostMapping("/form")
-    public String formSubmit(@ModelAttribute Board board) {
+    public String formSubmit(@Valid Board board, BindingResult bindingResult) {
+
+        boardValidator.validate(board, bindingResult);
+
+        if(bindingResult.hasErrors()) {
+            return "board/form";
+        }
+
         boardRepository.save(board);
         return "redirect:/board/list";
     }
