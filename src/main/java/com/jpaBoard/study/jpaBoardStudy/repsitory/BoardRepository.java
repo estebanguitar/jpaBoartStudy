b3ab7ead6a02898cadba939a39001a56b2d2ef82
wonmin.park <wonmin.park@kdac.io>
2024-11-04 09:18:33 +0900

    tmp

--- src/main/java/com/jpaBoard/study/jpaBoardStudy/repsitory/BoardRepository.java
+++ src/main/java/com/jpaBoard/study/jpaBoardStudy/repsitory/BoardRepository.java
@@ -1,7 +1,11 @@
 package com.jpaBoard.study.jpaBoardStudy.repsitory;
 
 import com.jpaBoard.study.jpaBoardStudy.entity.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+//  @Query(nativeQuery = true, value = "SELECT * FROM board WHERE upper(title) like '%#{titleKeyword}%' or upper(content) like")
+  List<Board> findByTitleContainingOrContentContaining(String titleKeyword, String contentKeyword);
 }
